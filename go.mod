2f96c7e364baf43d23252543b3e096431b214310
Christian Brintnall <christianbrintnall@gmail.com>
2019-06-28 19:00:03 -0700

    Continue work on job api

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/go-redis/redis v6.15.2+incompatible
 	k8s.io/api v0.0.0-20190626000116-b178a738ed00
+	k8s.io/apimachinery v0.0.0-20190624085041-961b39a1baa0
 	k8s.io/client-go v0.0.0-20190626045420-1ec4b74c7bda
 )
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  //"path/filepath"
-  "strconv"
-  "github.com/go-redis/redis"
-  "k8s.io/client-go/tools/clientcmd"
-  "k8s.io/client-go/kubernetes"
-  apiv1 "k8s.io/api/core/v1"
-  batchv1 "k8s.io/api/batch/v1"
-  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"fmt"
+	"os"
+	//"path/filepath"
+	"github.com/go-redis/redis"
+	batchv1 "k8s.io/api/batch/v1"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"strconv"
 )
 
 const REDIS_URL string = "0.0.0.0"
@@ -25,82 +25,82 @@ func homeDir() string {
 }
 
 func main() {
-  client := redis.NewClient(&redis.Options{
-    Addr: REDIS_URL + ":" + strconv.Itoa(REDIS_PORT),
-  })
+	client := redis.NewClient(&redis.Options{
+		Addr: REDIS_URL + ":" + strconv.Itoa(REDIS_PORT),
+	})
 
-  _, err := client.Ping().Result()
+	_, err := client.Ping().Result()
 
-  if err != nil {
-    panic("Error connecting to job's database")
-  }
+	if err != nil {
+		panic("Error connecting to job's database")
+	}
 
-  for /* ever */ {
-    // TODO: Make this activated on pubsub, this is going to hammer the server
-    jobs, err := client.Keys(JOB_KEY).Result()
+	for /* ever */ {
+		// TODO: Make this activated on pubsub, this is going to hammer the server
+		jobs, err := client.Keys(JOB_KEY).Result()
 
-    if err == nil {
-      for _, job := range jobs {
-        jid, err := client.Get(job).Result()
+		if err == nil {
+			for _, job := range jobs {
+				jid, err := client.Get(job).Result()
 
-        // If errors grabbing key, just skip iteration
-        if err != nil {
-          fmt.Printf("Unable to grab job payload for %s\n", job)
-          continue
-        }
+				// If errors grabbing key, just skip iteration
+				if err != nil {
+					fmt.Printf("Unable to grab job payload for %s\n", job)
+					continue
+				}
 
-        fmt.Printf("Starting job: %s\n", job)
-        go startJob(job, jid)
+				fmt.Printf("Starting job: %s\n", job)
+				go startJob(job, jid)
 
-        _, delErr := client.Del(job).Result()
+				_, delErr := client.Del(job).Result()
 
-        if delErr != nil {
-          fmt.Printf("Error removing job %s: %v\n", job, err)
-        }
-      }
-    }
-  }
+				if delErr != nil {
+					fmt.Printf("Error removing job %s: %v\n", job, err)
+				}
+			}
+		}
+	}
 }
 
 func startJob(key string, payload string) {
-  // TODO: Move kube stuff to method
-  kubeconfig := "./config"
+	// TODO: Move kube stuff to method
+	kubeconfig := "./config"
 
-  config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-  if err != nil {
-    fmt.Printf("ERR CREATING KUBECONFIG %v\n", err)
-  }
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		fmt.Printf("ERR CREATING KUBECONFIG %v\n", err)
+	}
 
-  clientset, kubeErr := kubernetes.NewForConfig(config)
-  if kubeErr != nil {
-    fmt.Printf("Kubernetes connecting failure")
-  }
+	clientset, kubeErr := kubernetes.NewForConfig(config)
+	if kubeErr != nil {
+		fmt.Printf("Kubernetes connecting failure")
+	}
 
-  jobClient := clientset.BatchV1().Jobs(apiv1.NamespaceDefault)
-  job := &batchv1.Job{
-    ObjectMeta: metav1.ObjectMeta{
-        Name: "demo-job",
-        Namespace: "default",
-    },
-    Spec: batchv1.JobSpec{
-        Template: apiv1.PodTemplateSpec{
-            Spec: apiv1.PodSpec{
-                RestartPolicy: "OnFailure",
-                Containers: []apiv1.Container{
-                    {
-                        Name:  "demo",
-                        Image: "myimage",
-                    },
-                },
-            },
-        },
-      },
-    }
+	jobClient := clientset.BatchV1().Jobs(apiv1.NamespaceDefault)
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "demo-job",
+			Namespace: "default",
+		},
+		Spec: batchv1.JobSpec{
+			Template: apiv1.PodTemplateSpec{
+				Spec: apiv1.PodSpec{
+					RestartPolicy: "OnFailure",
+					Containers: []apiv1.Container{
+						{
+							Name:  "demo",
+							Image: "myimage",
+						},
+					},
+				},
+			},
+		},
+	}
 
-  result, jobErr := jobClient.Create(job)
-  if jobErr == nil {
-    fmt.Printf("Job started: %v\n", result)
-  } else {
-    fmt.Printf("Error starting job: %v", jobErr)
-  }
+	result, jobErr := jobClient.Create(job)
+	if jobErr == nil {
+		fmt.Printf("Job started: %v\n", result)
+	} else {
+		fmt.Printf("Error starting job: %v", jobErr)
+	}
 }
